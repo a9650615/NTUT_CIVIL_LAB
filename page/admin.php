7302b0971fb927c7ab16e104f2ee0fc180ac47a9
菜鳥祐 <a9650615@gmail.com>
2018-01-09 14:29:16 +0800

    admin page

---
+++ page/admin.php
@@ -0,0 +1,18 @@
+<?php include './component/header.php'; ?>
+工程項目列表
+<a href="?page=create">建立</a>
+<table class="table">
+<thead>
+    <tr>
+        <th>工程名稱</th>
+        <th>狀態</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>項目工程1</td>
+        <td>未合格</td>
+    </tr>
+</tbody>
+</table>
+<?php include './component/footer.php'; ?>
