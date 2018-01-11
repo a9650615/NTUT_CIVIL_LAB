139cede6a9a600aa00735e2d47e316973e19fd15
菜鳥祐 <a9650615@gmail.com>
2018-01-11 16:50:51 +0800

    列表顯示

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    // error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    // error_reporting(E_ALL);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     if (!empty($_COOKIE['userId']) && !empty($_COOKIE['role'])) {
         $role = $_COOKIE['role'];
         if ($role == 1) {
--- page/admin.php
+++ page/admin.php
@@ -1,4 +1,9 @@
 <?php include './component/header.php'; ?>
+<?php
+    include './model/sql.php';
+    $sql = mysqli_query($conn, "SELECT * FROM quality_list");
+    $lists = $sql->fetch_assoc();
+?>
 工程項目列表
 <a href="?page=create_quality">建立</a>
 <table class="table">
@@ -9,10 +14,21 @@
     </tr>
 </thead>
 <tbody>
-    <tr>
-        <td>項目工程1</td>
-        <td>未合格</td>
-    </tr>
+    <?php
+        while ($data = $sql->fetch_assoc()) {
+            $status = "未改善";
+            if ($data['status'] == 1)
+                $status = "已改善";
+            if ($data['status'] == 2)
+                $status = "未合格";
+            ?>
+            <tr>
+                <td><?=$data['name']?></td>
+                <td><?=$status?></td>
+            </tr>
+            <?
+        }
+    ?>
 </tbody>
 </table>
 <?php include './component/footer.php'; ?>
