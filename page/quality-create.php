c622486684838f4dc17f76539278e6b478449295
菜鳥祐 <a9650615@gmail.com>
2018-01-22 00:27:16 +0800

    canvas 畫圖功能( 無上傳

--- page/quality-create.php
+++ page/quality-create.php
@@ -63,19 +63,24 @@
                         </tr>
                         <tr>
                             <td>施工現況<!--插入圖片-->
-                                <?php
-                                    if ($data['now_image']) {
-                                        ?>
-                                        <img id="output" required width="100%" src="upload_space/<?=$data['now_image']?>" />
-                                        <?php
-                                    } else {
-                                        ?>
-                                        <img id="output" required width="100%" style="display:none" />
-                                        <?php
-                                    }
-                                ?>
+                                <div style="position:relative;">
+                                    <canvas id="drawing" style="position:absolute; left: 0; height: 0; width:100%; height:100%;"></canvas>
+                                    <?php
+                                        if ($data['now_image']) {
+                                            ?>
+                                            <img id="output" required width="100%" src="upload_space/<?=$data['now_image']?>" />
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <img id="output" required width="100%" style="display:none" />
+                                            <?php
+                                        }
+                                    ?>
+                                </div>
                                 <input type="file" name="image" <?=($_GET['id']?"":"required")?> onchange="openFile(event)" accept="image/*"/>
                                 <script type="text/javascript">
+                                    let canvas = document.getElementById('drawing');
+                                    let ctx = canvas.getContext('2d');
                                         function openFile(event){
                                             var input = event.target; //取得上傳檔案
                                             var reader = new FileReader(); //建立FileReader物件
@@ -85,6 +90,75 @@
                                             $('#output').attr('src', dataURL).show(); //將img的src設定為dataURL並顯示
                                             };
                                         }
+                                    $('#output').load(() => {
+                                        canvas.width = $('#output').width()
+                                        canvas.height = document.getElementById('output').clientHeight;
+                                    })
+                                    // Set up mouse events for drawing
+                                    let drawing = false;
+                                    let mousePos = { x:0, y:0 };
+                                    let lastPos = mousePos;
+                                    canvas.addEventListener("mousedown", function (e) {
+                                            drawing = true;
+                                    lastPos = getMousePos(canvas, e);
+                                    }, false);
+                                    canvas.addEventListener("mouseup", function (e) {
+                                    drawing = false;
+                                    }, false);
+                                    canvas.addEventListener("mousemove", function (e) {
+                                    mousePos = getMousePos(canvas, e);
+                                    renderCanvas();
+                                    }, false);
+                                    
+                                    function renderCanvas() {
+                                        if (drawing) {
+                                            ctx.moveTo(lastPos.x, lastPos.y);
+                                            ctx.lineTo(mousePos.x, mousePos.y);
+                                            ctx.strokeStyle = '#ff0000';
+                                            ctx.lineWidth = 5;
+                                            ctx.stroke();
+                                            lastPos = mousePos;
+                                        }
+                                    }
+                                    // Get the position of the mouse relative to the canvas
+                                    function getMousePos(canvasDom, mouseEvent) {
+                                    var rect = canvasDom.getBoundingClientRect();
+                                    return {
+                                        x: mouseEvent.clientX - rect.left,
+                                        y: mouseEvent.clientY - rect.top
+                                    };
+                                    }
+                                        // Set up touch events for mobile, etc
+                                    // canvas.addEventListener("touchstart", function (e) {
+                                    //         mousePos = getTouchPos(canvas, e);
+                                    // var touch = e.touches[0];
+                                    // var mouseEvent = new MouseEvent("mousedown", {
+                                    //     clientX: touch.clientX,
+                                    //     clientY: touch.clientY
+                                    // });
+                                    // canvas.dispatchEvent(mouseEvent);
+                                    // }, false);
+                                    // canvas.addEventListener("touchend", function (e) {
+                                    // var mouseEvent = new MouseEvent("mouseup", {});
+                                    // canvas.dispatchEvent(mouseEvent);
+                                    // }, false);
+                                    // canvas.addEventListener("touchmove", function (e) {
+                                    // var touch = e.touches[0];
+                                    // var mouseEvent = new MouseEvent("mousemove", {
+                                    //     clientX: touch.clientX,
+                                    //     clientY: touch.clientY
+                                    // });
+                                    // canvas.dispatchEvent(mouseEvent);
+                                    // }, false);
+
+                                    // Get the position of a touch relative to the canvas
+                                    function getTouchPos(canvasDom, touchEvent) {
+                                    var rect = canvasDom.getBoundingClientRect();
+                                    return {
+                                        x: touchEvent.touches[0].clientX - rect.left,
+                                        y: touchEvent.touches[0].clientY - rect.top
+                                    };
+                                    }
                                 </script>
                             </td>
                         </tr>
