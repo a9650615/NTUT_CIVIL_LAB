e2eb55e5e29b9f786e22864e7a9c6fc6667e0cc8
菜鳥祐 <a9650615@gmail.com>
2018-02-06 14:25:00 +0800

    罰款更新功能

--- model/safty.php
+++ model/safty.php
@@ -9,7 +9,7 @@ switch ($_GET['action']) {
         $keys = array('missing_place', 'missing_company', 'missing_image', 'check_place', 'fine');
         break;
     case 'update':
-        $keys = array();
+        $keys = array('image');
         break;
 }
 
@@ -21,7 +21,6 @@ foreach ($keys as $key) {
 }
 
 if ($_GET['action'] == 'create' && $has_all_data) {
-    echo '1';
     $target_dir = "../upload_space/";
     $imageFileType = strtolower(pathinfo($_FILES["missing_image"]["name"],PATHINFO_EXTENSION));
     // $target_file = $target_dir . basename($_FILES["image"]["name"]);
@@ -35,6 +34,21 @@ if ($_GET['action'] == 'create' && $has_all_data) {
     $sql = mysqli_query($conn, "INSERT INTO safty_list(`missing_place`, `missing_company`, `check_place`, `fine`, `other`, `image`, `resolve_image`)
         VALUES ('{$_POST['missing_place']}', '{$_POST['missing_company']}', '{$_POST['check_place']}', '{$_POST['fine']}', '{$_POST['other']}', '{$file_name}', '')
     ");
+}
+
+if ($_GET['action'] == 'update' && $has_all_data) {
+    $target_dir = "../upload_space/";
+    $imageFileType = strtolower(pathinfo($_FILES["image"]["name"],PATHINFO_EXTENSION));
+    // $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $file_name = md5(microtime()) .'.'. $imageFileType;
+    $target_file = $target_dir . $file_name;
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        // echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+    } else {
+        // echo "Sorry, there was an error uploading your file.";
+    }
+    $sql = mysqli_query($conn, "UPDATE safty_list SET `resolve_image`='{$file_name}', `status`='3' WHERE ID='{$_GET['id']}'");
+    $page = "update_safty&id={$_GET['id']}";
 }
 
 header("Location: /?page={$page}");
--- page/update-safty.php
+++ page/update-safty.php
@@ -3,7 +3,6 @@
     <div class="row">
         <!--md=電腦 mm=手機 共12格 -->
       <div class="col-xs-12 col-sm-12 col-md-12">
-        <form action="/model/quality_list.php?action=update&id=<?=$_GET['id']?>" method="post" enctype="multipart/form-data">
             <div class="product_index">
 <?php
     include './model/sql.php';
@@ -16,12 +15,12 @@
             <form action="/model/safty.php?action=update&id=<?=$_GET['id']?>" method="post" enctype="multipart/form-data">
                 <table class="table" width="width:100%;">
                     <tr>
-                        <td>
+                        <td style="min-width:200px;">
                             缺失項目: <?=$data['missing_place']?><br>
                             缺失廠商: <?=$data['missing_company']?>
                         </td>
                         <td>
-                            更新照片：<input type="file" name="image" required/>
+                            現況照片: <img style="max-width:100%;" src="upload_space/<?=$data['image']?>" />
                         </td>
                     </tr>
                     <tr>
@@ -31,7 +30,14 @@
                             罰款項目: <?=$data['fine']?>
                         </td>
                         <td>
-                            
+                            <?php
+                                if ($data['resolve_image']) {
+                                    ?>
+                                    <img src="upload_space/<?=$data['resolve_image']?>" style="max-width:100%;" />
+                                    <?php
+                                }
+                            ?>
+                            更新照片：<input type="file" name="image" required/>
                         </td>
                     </tr>
                     <tr>
@@ -46,24 +52,23 @@
         <?php
     }
 ?>
-            </div>
-            <br>
-            <div style="font-size:25px;" class="col-sm-12 col-md-12 col-mm-12">
-                <input type="submit" id="sub" style="display: none;"/>
-                <?php
-                    if ($data && ($_COOKIE['role'] == 1 || $_COOKIE['role'] == 3)) {
-                        ?>
-                        <button type="button" onclick="upload()" class="button button-block" name="save" />確定更新</button>
-                        <?php
-                    } else if (!$data) {
-                        ?>
-                        找不到表單
-                        <?php
-                    }
-                ?>
-                <a href="?page=quality"><span style="float: right;" >回上一頁</span>
-            </div>
-        </form>
+        </div>
+        <br>
+        <div style="font-size:25px;" class="col-sm-12 col-md-12 col-mm-12">
+            <input type="submit" id="sub" style="display: none;"/>
+            <?php
+                if ($data && ($_COOKIE['role'] == 1 || $_COOKIE['role'] == 3)) {
+                    ?>
+                    <button type="button" onclick="upload()" class="button button-block" name="save" />確定更新</button>
+                    <?php
+                } else if (!$data) {
+                    ?>
+                    找不到表單
+                    <?php
+                }
+            ?>
+            <a href="?page=safty"><span style="float: right;" >回上一頁</span>
+        </div>
       </div>
     </div>
 </div>
