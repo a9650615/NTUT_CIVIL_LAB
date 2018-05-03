42b00b2bb5f3c83faa3614755d53d2d6a63dc44c
菜鳥祐 <a9650615@gmail.com>
2018-05-04 03:38:25 +0800

    safty 年 月份 調整

--- page/safty-statistics.php
+++ page/safty-statistics.php
@@ -1,13 +1,23 @@
 <?php include './component/header.php'; ?>
 <?php
     include './model/sql.php';
-    $in_one_month = "WHERE MONTH(check_date) = MONTH(CURRENT_DATE()) AND YEAR(check_date) = YEAR(CURRENT_DATE())";
+    $year = $_GET['year']? $_GET['year']: date('Y');
+    $month = $_GET['month']? $_GET['month']: date('m');
+    if ($_GET['year']) {
+        $timestamp = strtotime("{$_GET['year']}/{$month}/1");
+    } else {
+        $timestamp = time();
+    }
+    $in_one_month = "WHERE MONTH(check_date) = MONTH('{$year}-{$month}-1') AND YEAR(check_date) = YEAR('{$year}-{$month}-1')";
+    // echo $in_one_month;
     $data_month_total = mysqli_query($conn, "SELECT * FROM safty_list {$in_one_month}");
     $data_month_finish = mysqli_query($conn, "SELECT * FROM safty_list {$in_one_month} AND status=1");
     $data_month_outdate = mysqli_query($conn, "SELECT * FROM safty_list {$in_one_month} AND status!=1 AND check_date < now()");
-    $season = 4 - intval(12/date('m'));
-    $season_start = date('Y')."-".($season*4)."-1";
-    $season_end = date('Y')."-".($season*4+4)."-30";
+    $season = 4 - intval((12-date('m', $timestamp)) / 3)-1;
+    $season_start = date('Y', $timestamp)."-".($season*3+1)."-1";
+    $season_end = date('Y', $timestamp)."-".($season*3+3)."-30";
+    // echo intval((12-date('m', $timestamp)) / 3)+1;
+    // echo date('m', $timestamp).'---'.$season_start.'---'.$season_end;
     $in_one_season = "WHERE check_date BETWEEN '{$season_start}' AND '{$season_end}'";
     $data_season_total = mysqli_query($conn, "SELECT * FROM safty_list {$in_one_season}");
     $data_season_finish = mysqli_query($conn, "SELECT * FROM safty_list {$in_one_season} AND status=1");
@@ -20,7 +30,11 @@
 ?>
 <a href="?page=safty">上一頁</a>
 <div class="col-sm-12 col-md-12 col-mm-12 container">
-    <h2>月/季/年統計</h2>
+    <a href="?page=<?=$_GET['page']?>&year=<?=date('Y', $timestamp) - 1?>">上一年</a>
+    <a href="?page=<?=$_GET['page']?>&year=<?=date('m', $timestamp)==1?date('Y', $timestamp)-1:date('Y', $timestamp)?>&month=<?=date('m', $timestamp)==1?12:date('m', $timestamp)-1?>">上一月 ( 單一月份 )</a>
+    <a href="?page=<?=$_GET['page']?>&year=<?=date('m', $timestamp)==12?date('Y', $timestamp)+1:date('Y', $timestamp)?>&month=<?=date('m', $timestamp)==12?1:date('m', $timestamp)+1?>">下一月 ( 單一月份 )</a>
+    <a href="?page=<?=$_GET['page']?>&year=<?=date('Y', $timestamp) + 1?>">下一年</a>
+    <h2><?=$month?>月/<?=$season+1?>季/<?=$year?>年統計</h2>
     <table class="table">
         <thead>
             <tr>
